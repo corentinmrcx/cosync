e1ed729193e0463af59e33fd91f2f1f9e6a8e02d
Corentin Marcoux <corentinmarcoux51@gmail.com>
2026-06-28 15:56:38 +0200

    feat: entités modèles et affectations de dotation

---
+++ src/Repository/DotationAffectationRepository.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\DotationAffectation;
+use App\Entity\Season;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<DotationAffectation>
+ */
+class DotationAffectationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, DotationAffectation::class);
+    }
+
+    /** @return DotationAffectation[] Toutes les affectations de la saison, jointures préchargées. */
+    public function findBySeason(Season $season): array
+    {
+        return $this->createQueryBuilder('a')
+            ->leftJoin('a.modele', 'm')->addSelect('m')
+            ->leftJoin('a.category', 'c')->addSelect('c')
+            ->leftJoin('a.team', 't')->addSelect('t')
+            ->leftJoin('a.licencie', 'l')->addSelect('l')
+            ->leftJoin('a.dirigeant', 'd')->addSelect('d')
+            ->where('a.season = :season')
+            ->setParameter('season', $season)
+            ->getQuery()
+            ->getResult();
+    }
+}
